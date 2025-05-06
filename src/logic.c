584340e8cfe211ca8691361ca8912ce8a7c48cea
thmaz <thomasplmn@gmail.com>
2025-05-06 10:36:17 +0200

    init

---
+++ src/logic.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAXOP   100
+#define NUMBER  '0' //Signal for found number  
+
+void push(double f);
+double pop(void);
+int getop(char s[]);
+
+/* Entry for reverse polish calculator */
+int main() {
+
+}
