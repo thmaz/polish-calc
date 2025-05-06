8f8ae12a3a9b5e56461542d8bf0112840ed903e7
thmaz <thomasplmn@gmail.com>
2025-05-06 10:50:22 +0200

    main logic

--- src/logic.c
+++ src/logic.c
@@ -4,11 +4,52 @@
 #define MAXOP   100
 #define NUMBER  '0' //Signal for found number  
 
+
+/*  Reverse Polish calculator:
+    Each operand is pushed onto stack
+    When operator arrives, valid num of operands is popped, then the operator is applied
+    Result gets pushed back onto stack
+*/
+
+
 void push(double f);
 double pop(void);
 int getop(char s[]);
 
-/* Entry for reverse polish calculator */
 int main() {
+    int type;
+    double op2;
+    char s[MAXOP];
 
+    while ((type = getop(s)) != EOF) {
+        switch (type) {
+        case NUMBER:
+            push(atof(s));
+            break;
+        case '+':
+            push(pop() + pop());
+            break;
+        case '*':
+            push(pop() * pop());
+            break;
+        case '-':
+            op2 = pop();
+            push(pop() - op2);
+            break;
+        case '/':
+            op2 = pop();
+            if (op2 != 0.0)
+                push(pop() / op2);
+            else
+             printf("error: zero divisor\n");
+            break;     
+        case '\n':
+            printf("\t.8g\n", pop());
+            break;
+        default:
+            printf("error: unknown command %s\n", s);
+            break;
+        }
+        return 0;
+    }
 }
